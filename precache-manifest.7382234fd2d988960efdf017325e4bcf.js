a24072d7c75f602f05c91c72ea2b86a45f19c7ae
buingoctai <buingoctai994@gmail.com>
2020-09-04 11:07:22 +0700

    Updates

---
+++ precache-manifest.7382234fd2d988960efdf017325e4bcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce8643a8394f35dd961ad007aae965c",
+    "url": "/preview-drag/index.html"
+  },
+  {
+    "revision": "958c7f751ef27cc498f0",
+    "url": "/preview-drag/static/css/main.5ece22a0.chunk.css"
+  },
+  {
+    "revision": "a37938fe39187fb2a4a7",
+    "url": "/preview-drag/static/js/2.116bb3f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/preview-drag/static/js/2.116bb3f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "958c7f751ef27cc498f0",
+    "url": "/preview-drag/static/js/main.ac27ca8f.chunk.js"
+  },
+  {
+    "revision": "6469bc26c053a4a937dc",
+    "url": "/preview-drag/static/js/runtime-main.fff19ed1.js"
+  }
+]);
